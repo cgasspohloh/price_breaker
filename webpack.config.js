74e82183865472051ba286b46181ab07640b0e4c
Curtis Gass <curtis@pohloh.com>
2023-12-15 09:25:06 -0500

    setup for rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production', // or 'development'
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  // Add any necessary plugins or optimizations here
 };
